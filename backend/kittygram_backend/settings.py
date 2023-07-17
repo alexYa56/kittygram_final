d0cb11bbdc856015a2dec39b824d9a9717f0dc4f
Alexandr Yastrebov <a.v.yastrebov86@yandex.ru>
2023-07-17 18:34:41 +0300

    refresh ALLOWED

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,11 +7,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('TOKEN')
 
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(', ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
